d5bbf501c511066d1aa2db68e9a91a2a82c2d6d9
pilpan <plip2010195@gmail.com>
2022-09-22 00:49:36 +0300

    fix hightSeverity

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ buttonEl.addEventListener('click',() =>{
     let oncoDeseases = document.getElementById('oncoDeseases')
     let sex = document.getElementById('sex')
     let hightSeverity = document.getElementById('hightSeverity');
-    let midleSeveruty = document.getElementById("midleSeveruty");
     let fever = document.getElementById('fever');
     let tasteLoss = document.getElementById('tasteLoss');
     let rhinitis = document.getElementById("rhinitis");
@@ -19,7 +18,7 @@ buttonEl.addEventListener('click',() =>{
     let covContact = document.getElementById("covContact");
     let resEl = document.getElementById("result")
 
-    let coeffz = -1.010 + 4.224 * hightSeverity.value + 2.123 * midleSeveruty.value + 0.403 * oncoDeseases.value + 0.406 * otherDeceases.value + 0.423 * dyspnea.value + 0.397 * hospital.value + 0.453 * endocrineDeceases.value + 0.228 * sex.value + 0.407 * heartDeseases.value + 0.364 * lungDeseases.value + 0.012 * age.value + 0.355 * fever.value - 0.512 * rhinitis.value - 0.377 * tasteLoss.value - 0.377 * covContact.value - 1.352 * clinic.value;
+    let coeffz = -1.010 + hightSeverity.value + 0.403 * oncoDeseases.value + 0.406 * otherDeceases.value + 0.423 * dyspnea.value + 0.397 * hospital.value + 0.453 * endocrineDeceases.value + 0.228 * sex.value + 0.407 * heartDeseases.value + 0.364 * lungDeseases.value + 0.012 * age.value + 0.355 * fever.value - 0.512 * rhinitis.value - 0.377 * tasteLoss.value - 0.377 * covContact.value - 1.352 * clinic.value;
     let res = CalculateProbability(coeffz);
     resEl.innerHTML += `<p>${res}</p>`
     if (res < 0.669) {
